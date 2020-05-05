f7a92818647d3722898d864eef6f9631324ab559
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-05 20:23:56 +1000

    Solved Day 5 Part 2
    Refactored bulk of D5_P1 solver into separate function, to be reused
    in D5_P2 solver.

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[aoc_generator(day5)]
 fn generate_input(input: &str) -> Vec<char> {
     return input.trim().chars().collect::<Vec<char>>();
@@ -5,26 +7,57 @@ fn generate_input(input: &str) -> Vec<char> {
 
 #[aoc(day5, part1)]
 fn solve_part_1(input: &Vec<char>) -> usize {
-    let mut polym = input.clone();
+    let mut polymer = react_polymer(input);
+    return polymer.len();
+}
+
+#[aoc(day5, part2)]
+fn solve_part_2(input: &Vec<char>) -> usize {
+    // Get all unique unit types in the input polymer
+    let mut unit_types: HashSet<char> = input
+        .into_iter()
+        .map(|x| x.to_ascii_lowercase())
+        .collect::<HashSet<char>>();
+    // Keep track of min length observed
+    let mut min_polymer_seen = usize::MAX;
+    for unit_type in unit_types {
+        let mut test_polymer = input.clone();
+        // Remove all instances of current unit type from test polymer
+        test_polymer.retain(|x| *x != unit_type && *x != unit_type.to_ascii_uppercase());
+        // Fully react the test polymer and check if it is the shortest seen so far
+        let reacted_polymer = react_polymer(&test_polymer);
+        if reacted_polymer.len() < min_polymer_seen {
+            min_polymer_seen = reacted_polymer.len();
+        }
+    }
+    return min_polymer_seen;
+}
+
+/// Fully reacts the given input polymer, progressively destroying all type-pairs of opposite
+/// polarity.
+fn react_polymer(input_poly: &Vec<char>) -> Vec<char> {
+    let mut polymer = input_poly.clone();
     // Start with first pair of characters
     let mut i = 0;
     loop {
         // Terminate when end of string is reached
-        if i == polym.len() - 1 || polym.len() <= 2 {
+        if i == polymer.len() - 1 || polymer.len() <= 2 {
             break;
         }
         // If pair destroys each other, remove the pair and decrement pair index
-        if polym[i] != polym[i + 1] 
-            && (polym[i + 1] == polym[i].to_ascii_uppercase()
-                || polym[i + 1] == polym[i].to_ascii_lowercase()) {
-            polym.remove(i);
-            polym.remove(i);
+        if polymer[i] != polymer[i + 1]
+            && (polymer[i + 1] == polymer[i].to_ascii_uppercase()
+                || polymer[i + 1] == polymer[i].to_ascii_lowercase())
+        {
+            polymer.remove(i);
+            polymer.remove(i);
             if i > 0 {
                 i -= 1;
             }
-        } else { // If pair does not destroy each other, increment pair index
+        } else {
+            // If pair does not destroy each other, increment pair index
             i += 1;
         }
     }
-    return polym.len();
+    return polymer;
 }

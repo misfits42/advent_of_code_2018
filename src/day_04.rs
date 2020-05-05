052860c4b7938b7c62834aa1b0f5dde15021bb35
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-05 19:45:14 +1000

    Solved Day4 Part2

--- src/day_04.rs
+++ src/day_04.rs
@@ -81,28 +81,28 @@ fn solve_part_1(input: &Vec<GuardWatch>) -> u64 {
     let mut sleepiest_guard = 0;
     let mut max_sleep = 0;
     // Process each sleep period from each guard watch
-    for guard_watch in input {
-        for sleep_period in &guard_watch.sleep_periods {
+    for watch in input {
+        for sleep_period in &watch.sleep_periods {
             let sleep_mins = calculate_sleep_period_minutes(sleep_period);
             // Check if guard has already been seen
-            if sleep_sheets.contains_key(&guard_watch.id) {
-                let existing_sleep_sheet = sleep_sheets.get_mut(&guard_watch.id).unwrap();
+            if sleep_sheets.contains_key(&watch.id) {
+                let existing_sleep_sheet = sleep_sheets.get_mut(&watch.id).unwrap();
                 for i in sleep_mins.0..sleep_mins.1 {
                     *existing_sleep_sheet.get_mut(&i).unwrap() += 1;
                 }
-                *sleep_totals.get_mut(&guard_watch.id).unwrap() += sleep_mins.1 - sleep_mins.0;
+                *sleep_totals.get_mut(&watch.id).unwrap() += sleep_mins.1 - sleep_mins.0;
             } else { // Add guard sleep times new
                 let mut new_sleep_sheet: HashMap<u64, u64> = create_new_guard_sleep_sheet();
                 for i in sleep_mins.0..sleep_mins.1 {
                     *new_sleep_sheet.get_mut(&i).unwrap() += 1;
                 }
-                sleep_sheets.insert(guard_watch.id, new_sleep_sheet);
-                sleep_totals.insert(guard_watch.id, sleep_mins.1 - sleep_mins.0);
+                sleep_sheets.insert(watch.id, new_sleep_sheet);
+                sleep_totals.insert(watch.id, sleep_mins.1 - sleep_mins.0);
             }
             // Check if we have a new max sleep value - have we found a more sleepy guard?
-            if *sleep_totals.get(&guard_watch.id).unwrap() > max_sleep {
-                max_sleep = *sleep_totals.get(&guard_watch.id).unwrap();
-                sleepiest_guard = guard_watch.id;
+            if *sleep_totals.get(&watch.id).unwrap() > max_sleep {
+                max_sleep = *sleep_totals.get(&watch.id).unwrap();
+                sleepiest_guard = watch.id;
             }
         }
     }
@@ -116,6 +116,50 @@ fn solve_part_1(input: &Vec<GuardWatch>) -> u64 {
         }
     }
     return sleepiest_guard * max_sleep_minute;
+}
+
+#[aoc(day4, part2)]
+fn solve_part_2(input: &Vec<GuardWatch>) -> u64 {
+    // Keep track of guard sleep sheets
+    let mut sleep_sheets: HashMap<u64, HashMap<u64, u64>> = HashMap::new();
+    // Keep track of target guard and sleep minute
+    let mut target_guard = 0;
+    let mut target_minute = 0;
+    let mut max_minute_sleep = 0;
+    // Process each guard watch and sleep period contained within
+    for watch in input {
+        for sleep_period in &watch.sleep_periods {
+            let sleep_mins = calculate_sleep_period_minutes(sleep_period);
+            // Check if guard already seen
+            if sleep_sheets.contains_key(&watch.id) {
+                let existing_sleep_sheet = sleep_sheets.get_mut(&watch.id).unwrap();
+                for minute in sleep_mins.0..sleep_mins.1 {
+                    // Increment sleep for minute
+                    *existing_sleep_sheet.get_mut(&minute).unwrap() += 1;
+                    // Check if incremented sleep makes current guard the target
+                    if *existing_sleep_sheet.get(&minute).unwrap() > max_minute_sleep {
+                        max_minute_sleep = *existing_sleep_sheet.get(&minute).unwrap();
+                        target_minute = minute;
+                        target_guard = watch.id;
+                    }
+                }
+            } else { // Add new guard to sleep sheets
+                let mut new_sleep_sheet: HashMap<u64, u64> = create_new_guard_sleep_sheet();
+                for minute in sleep_mins.0..sleep_mins.1 {
+                    *new_sleep_sheet.get_mut(&minute).unwrap() += 1;
+                    // Check if a new target guard has been found
+                    if *new_sleep_sheet.get(&minute).unwrap() > max_minute_sleep {
+                        max_minute_sleep = *new_sleep_sheet.get(&minute).unwrap();
+                        target_minute = minute;
+                        target_guard = watch.id;
+                    }
+                }
+                // Add new guard sleep sheet to tracker
+                sleep_sheets.insert(watch.id, new_sleep_sheet);
+            }
+        }
+    }
+    return target_guard * target_minute;
 }
 
 /// Extracts the minutes field from the two timestamps in the sleep period.

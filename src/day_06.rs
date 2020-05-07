1c9334c987b1e159c3b4719c810b61c9b386b75d
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-07 22:15:11 +1000

    Implemented a possible solution for Day 6 Part 2
    Solution is too memory intensive - need to think of a better solution

--- src/day_06.rs
+++ src/day_06.rs
@@ -101,3 +101,46 @@ fn solve_part_1(input: &HashMap<i64, Point2D>) -> u64 {
         .unwrap();
     return max_area_size;
 }
+
+#[aoc(day6, part2)]
+fn solve_part_2(input: &HashMap<i64, Point2D>) -> usize {
+    // Initialise empty hashmap to total manhattan distance to each known location from points
+    let mut distance_count: HashMap<Point2D, u64> = HashMap::new();
+    let distance = 10000;
+    // Process each point in input
+    for (_, point) in input.iter() {
+        // Find the points within set Manhattan distance of current point
+        // let close_points = point.calculate_points_within_manhattan_distance(10000);
+
+        for pos_x in (point.pos_x - distance)..(point.pos_x + distance + 1) {
+            // Calculate how far above and below central x-axis to go
+            let y_extra = (pos_x - point.pos_x + distance).abs();
+            for pos_y in (point.pos_y - y_extra)..(point.pos_y + y_extra + 1) {
+                // Calculate the current point and its distance from the self point
+                let point = Point2D::new(pos_x, pos_y);
+                let distance_from_loc: u64 = ((point.pos_x - pos_x).abs() + (point.pos_y - pos_y).abs()) as u64;
+                // Check if point with same distance has been found already
+                if distance_count.contains_key(&point) {
+                    *distance_count.get_mut(&point).unwrap() += distance_from_loc;
+                } else {
+                    distance_count.insert(point, distance_from_loc);
+                }
+            }
+        }
+
+        // for (distance, near_points) in close_points.iter() {
+        //     // For each point at same distance from current point, add its distance total into
+        //     // overall distance count
+        //     for near_point in near_points {
+        //         if distance_count.contains_key(&near_point) {
+        //             *distance_count.get_mut(&near_point).unwrap() += distance;
+        //         } else {
+        //             distance_count.insert(*near_point, *distance);
+        //         }
+        //     }
+        // }
+    }
+    distance_count.retain(|_k, v| v < &mut 10000);
+    let area_size = distance_count.keys().len();
+    return area_size;
+}

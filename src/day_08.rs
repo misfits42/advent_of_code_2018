57103b29f8449a4ba83521a064066af84ca960f0
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-10 22:38:29 +1000

    Solved Day 8 Part 2
    Day 8 was surprisingly challenging.
    
    As I was working on my solution for Part 2, I realised I had a bug in
    how I was assigning Node IDs when creating the initial licence graph.
    (Bug did not impact the Part 1 solution initially). To solve this
    issue, I decided to use a mutable variable to track the last Node ID
    assigned and pass a mutable reference to the var with each resursive
    function call processing the licence file input.
    
    Once I ironed out the bugs with my generator function, I came to a
    reasonable solution for Part 2 after some effort. Reasonably happy
    with the solution, but might tidy up at some point for neatness-sake.

--- src/day_08.rs
+++ src/day_08.rs
@@ -1,11 +1,12 @@
 use petgraph::graphmap::DiGraphMap;
+use petgraph::Direction;
 
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
 struct LicenceGraph {
     graph: DiGraphMap<i64, i64>,
-    node_data: HashMap<i64, Vec<i64>>,
+    node_data: HashMap<i64, Vec<i64>>
 }
 
 impl LicenceGraph {
@@ -16,14 +17,48 @@ impl LicenceGraph {
         }
     }
 
+    /// Calculates the sum of all metadata values contained within the licence graph.
+    /// 
+    /// Used in Day 8 Part 1 solution.
     pub fn get_metadata_sum(&self) -> i64 {
-        let mut result = 0;
+        let mut result: i64 = 0;
         for entries in self.node_data.values() {
             for entry in entries {
                 result += entry;
             }
         }
         return result;
+    }
+
+    /// Calculates the total value of the root node.
+    /// 
+    /// Used in Day 8 Part 2 solution.
+    pub fn get_root_node_value(&self) -> i64 {
+        // Root node has ID of 0
+        return self.get_node_value(0);
+    }
+
+    fn get_node_value(&self, node_id: i64) -> i64 {
+        let num_children = self.graph.neighbors_directed(node_id, Direction::Outgoing).count();
+        if num_children == 0 {
+            return self.node_data.get(&node_id).unwrap().iter().sum();
+        } else {
+            let mut child_ids = self.graph.neighbors_directed(node_id, Direction::Outgoing).collect::<Vec<i64>>();
+            child_ids.sort();
+            let mut child_meta_sum = 0;
+            for meta in self.node_data.get(&node_id).unwrap() {
+                // Ignore metadata values of 0 - they do not correspond to a child node
+                if *meta == 0 {
+                    continue;
+                }
+                let index = (meta - 1) as usize;
+                // Check if the index relates to one of the child nodes - otherwise ignore
+                if index < child_ids.len() {
+                    child_meta_sum += self.get_node_value(child_ids[index]);
+                }
+            }
+            return child_meta_sum;
+        }
     }
 }
 
@@ -35,17 +70,22 @@ fn generate_input(input: &str) -> LicenceGraph {
         .split_whitespace()
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<VecDeque<i64>>();
-    //// println!("{:?}", licence_nums);
     // Create empty licence graph
     let mut licence_graph = LicenceGraph::new();
     // Process the licence graph
-    process_licence_nums(&mut licence_nums, &mut licence_graph, -1);
+    let mut last_id = -1;
+    process_licence_nums(&mut licence_nums, &mut licence_graph, &mut last_id, -1);
     return licence_graph;
 }
 
-#[aoc(day8. part1)]
+#[aoc(day8, part1)]
 fn solve_part_1(input: &LicenceGraph) -> i64 {
     return input.get_metadata_sum();
+}
+
+#[aoc(day8, part2)]
+fn solve_part_2(input: &LicenceGraph) -> i64 {
+    return input.get_root_node_value();
 }
 
 /// Extracts nodes from the licence numbers sequence and adds the node and metadata entries to the
@@ -55,15 +95,18 @@ fn solve_part_1(input: &LicenceGraph) -> i64 {
 fn process_licence_nums(
     licence_nums: &mut VecDeque<i64>,
     licence_graph: &mut LicenceGraph,
-    parent_id: i64,
-) -> i64 {
-    let mut current_id = parent_id;
+    last_id: &mut i64,
+    parent_id: i64
+) {
+    // Increment last ID and set current ID
+    *last_id += 1;
+    let current_id = *last_id;
     // Get number child items and metadeta entries
     let num_child = licence_nums.pop_front().unwrap();
     let num_metadata = licence_nums.pop_front().unwrap();
     // Recursively extract each child
     for _i in 0..num_child {
-        current_id = process_licence_nums(licence_nums, licence_graph, current_id);
+        process_licence_nums(licence_nums, licence_graph, last_id, current_id);
     }
     // Get the metadata entries
     let mut metadata_entries: Vec<i64> = vec![];
@@ -71,12 +114,10 @@ fn process_licence_nums(
         metadata_entries.push(licence_nums.pop_front().unwrap());
     }
     // Add node and metadata entry to graph
-    current_id += 1;
     licence_graph.node_data.insert(current_id, metadata_entries.to_vec());
     licence_graph.graph.add_node(current_id);
     // Add parent-child rel if we are not the root node
     if parent_id != -1 {
         licence_graph.graph.add_edge(parent_id, current_id, -1);
     }
-    return current_id;
 }

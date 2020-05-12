4527a2009d0d3df627657c082d3a55651bb5fbc6
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-12 20:05:03 +1000

    Day 11 - removed first subgrid power function
    Replaced usage of first subgrid power function (no convolution) with
    the improved version developed during Part 2. Usage of improved alg
    in Part 1 provided a small performance improvement (~7ms - down to
    17ms).

--- src/day_11.rs
+++ src/day_11.rs
@@ -17,20 +17,22 @@ fn generate_input(input: &str) -> HashMap<Point2D, i64> {
 }
 
 #[aoc(day11, part1)]
-fn solve_part_1(cell_grid: &HashMap<Point2D, i64>) -> Point2D {
+fn solve_part_1(cell_grid: &HashMap<Point2D, i64>) -> String {
     let mut max_power_seen = 0;
     let mut max_top_left = Point2D::new(-1, -1);
     for pos_y in 1..299 {
+        let mut last_sub_power = 0;
         for pos_x in 1..299 {
             let top_left = Point2D::new(pos_x, pos_y);
-            let subgrid_power = find_subgrid_power(cell_grid, &top_left, 3);
+            let subgrid_power = find_subgrid_power(cell_grid, 3, last_sub_power, &top_left);
             if subgrid_power > max_power_seen {
                 max_power_seen = subgrid_power;
                 max_top_left = top_left;
             }
+            last_sub_power = subgrid_power;
         }
     }
-    return max_top_left;
+    return format!("{},{}", max_top_left.pos_x, max_top_left.pos_y);
 }
 
 #[aoc(day11, part2)]
@@ -52,21 +54,21 @@ fn solve_part_2(cell_grid: &HashMap<Point2D, i64>) -> String {
                 }
                 // Calculate the current subgrid power by shifting last subgrid across one column
                 let top_left = Point2D::new(pos_x, pos_y);
-                let subgrid_power = find_subgrid_power_shift(
+                let subgrid_power = find_subgrid_power(
                     cell_grid,
                     width,
                     last_sub_power,
                     &top_left,
                 );
+                last_sub_power = subgrid_power;
+                // Check if we have found a new max power
                 if subgrid_power > max_power_seen {
                     max_power_seen = subgrid_power;
                     max_top_left = top_left;
                     size_of_max = width;
                 }
-                last_sub_power = subgrid_power;
             }
         }
-        // println!("Completed width {}. Current max power: {}", width, max_power_seen);
     }
     return format!(
         "{},{},{}",
@@ -74,7 +76,7 @@ fn solve_part_2(cell_grid: &HashMap<Point2D, i64>) -> String {
     );
 }
 
-fn find_subgrid_power_shift(
+fn find_subgrid_power(
     cell_grid: &HashMap<Point2D, i64>,
     width: i64,
     last_sub_power: i64,
@@ -83,6 +85,7 @@ fn find_subgrid_power_shift(
     // Check if we have overlap - subtract the difference
     if top_left.pos_x == 1 {
         let mut subgrid_power = 0;
+        // Calculate new row subgrid by summing ALL power values within test subgrid
         for delta_y in 0..width {
             for delta_x in 0..width {
                 let test_point = top_left.move_point(delta_x, delta_y);
@@ -91,9 +94,11 @@ fn find_subgrid_power_shift(
         }
         return subgrid_power;
     } else {
+        // Calculate the x-value of the last column of the current subgrid
         let last_x = top_left.pos_x + width - 1;
         let mut subgrid_power = last_sub_power;
         for pos_y in top_left.pos_y..(top_left.pos_y + width) {
+            // Calculate points for powers to subtract and add
             let subtract_point = Point2D::new(top_left.pos_x - 1, pos_y);
             let add_point = Point2D::new(last_x, pos_y);
             subgrid_power -= cell_grid.get(&subtract_point).unwrap();
@@ -103,26 +108,12 @@ fn find_subgrid_power_shift(
     }
 }
 
-/// Adds up the power of all cells within the square of specified width starting from the given
-/// location.
-fn find_subgrid_power(cell_grid: &HashMap<Point2D, i64>, top_left: &Point2D, width: i64) -> i64 {
-    let mut subgrid_power = 0;
-    // Add together the powers of each cell in 3x3 grid with given point as top left
-    for delta_y in 0..width {
-        for delta_x in 0..width {
-            let test_point = top_left.move_point(delta_x, delta_y);
-            subgrid_power += cell_grid.get(&test_point).unwrap();
-        }
-    }
-    return subgrid_power;
-}
-
-fn calculate_power_level(serial_no: i64, cell_loc: &Point2D) -> i64 {
+fn calculate_power_level(serial_num: i64, cell_loc: &Point2D) -> i64 {
     // Find rack ID
     let rack_id = cell_loc.pos_x + 10;
     // Calculate power level
     let mut power_level: i64 = rack_id * cell_loc.pos_y;
-    power_level += serial_no;
+    power_level += serial_num;
     power_level *= rack_id;
     power_level = (power_level % 1000) / 100;
     power_level -= 5;

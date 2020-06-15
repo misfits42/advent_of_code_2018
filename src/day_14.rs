ee37557c1b7bc8038905513920aeb72859f72ba3
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-16 09:13:12 +1000

    Solved AoC 2018 Day 14, Part 2
    - Worked out I wasn't checking one of the edge cases. The P2 substring could
      appear at the end of the recipe string or offset from the end by one.

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,3 +1,5 @@
+/// This struct represents the hot chocolate recipe scoreboard introducted in the AoC 2018 Day 14
+/// challenge.
 struct RecipeBoard {
     recipes: String,
     num_recipes: usize,
@@ -6,6 +8,7 @@ struct RecipeBoard {
 }
 
 impl RecipeBoard {
+    /// Creates a new RecipeBoard with the given starting recipes.
     pub fn new(orig_recipe: &str) -> Self {
         if orig_recipe.chars().count() < 2 {
             panic!("Day14 - initial recipe length is insufficient.");
@@ -18,12 +21,14 @@ impl RecipeBoard {
         }
     }
 
+    /// Extracts the current recipe scores for the two elves.
     fn get_elf_current_recipes(&self) -> (usize, usize) {
         let elf_one = self.recipes.get(self.elf_one_i..self.elf_one_i+1).unwrap().parse::<usize>().unwrap();
         let elf_two = self.recipes.get(self.elf_two_i..self.elf_two_i+1).unwrap().parse::<usize>().unwrap();
         return (elf_one, elf_two);
     }
 
+    /// Updates the current recipe locations of the two elves based on their current recipe scores.
     fn update_elf_current_recipes(&mut self) {
         let elf_current_recipes = self.get_elf_current_recipes();
         self.elf_one_i = (self.elf_one_i + elf_current_recipes.0 + 1) % self.num_recipes;
@@ -35,41 +40,48 @@ impl RecipeBoard {
         let elf_current_recipes = self.get_elf_current_recipes();
         // Calculate the new recipes
         let new_recipes = elf_current_recipes.0 + elf_current_recipes.1;
-        // Add new recipes to the board
-        self.recipes.extend(new_recipes.to_string().chars()); // += &new_recipes.to_string();
-        // println!("!!! {} / 10 + 1 = {}", new_recipes, (new_recipes / 10) + 1);
+        // Add new recipes to the board. (int div rounds down).
+        self.recipes.extend(new_recipes.to_string().chars());
         self.num_recipes += (new_recipes / 10) + 1;
         // Update the current recipe for both elves
         self.update_elf_current_recipes();
     }
 
-    pub fn conduct_turns_until(&mut self, recipes_to_left: usize) -> String {
+    /// Conducts turns until there is at least the given number of recipes + 10 in the recipe list.
+    /// 
+    /// Returns the sequence of 10 recipes immediately after the given number of recipes.
+    pub fn conduct_turns_until(&mut self, to_left: usize) -> String {
         loop {
             // Check if we have generated enough new recipes
-            if self.num_recipes >= (recipes_to_left + 10) {
-                break;
+            if self.num_recipes >= (to_left + 10) {
+                return String::from(self.recipes.get(to_left..(to_left + 10)).unwrap());
             }
             // Conduct a new turn to generate new recipes
             self.conduct_turn();
         }
-        let result = self.recipes.get(recipes_to_left..(recipes_to_left + 10)).unwrap();
-        return String::from(result);
     }
 
-    pub fn conduct_turns_until_appearance(&mut self, appearance: &str) -> usize {
-        let mut count: u128 = 0;
+    /// Conducts turns until the given score sequence appears in the recipe list. The score sequence
+    /// will either appear at the end of the recipe list or one offset from the end.
+    /// 
+    /// Returns the number of recipes to the left of the first appearance of the given score
+    /// sequence.
+    pub fn conduct_turns_until_appearance(&mut self, score_seq: &str) -> usize {
+        let seq_len = score_seq.chars().count();
         loop {
-            // Check if the given sequence is at end of recipes list
-            if self.recipes.ends_with(appearance) {
-                return self.num_recipes - appearance.chars().count();
+            // Check if given sequence is either at end or end offset by 1
+            if self.num_recipes > seq_len {
+                // Get the string at end and set back by 1
+                let end = self.recipes.get((self.num_recipes - seq_len)..self.num_recipes).unwrap();
+                let end_1 = self.recipes.get((self.num_recipes - seq_len - 1)..(self.num_recipes-1)).unwrap();
+                if score_seq == end {
+                    return self.num_recipes - seq_len
+                } else if score_seq == end_1 {
+                    return self.num_recipes - seq_len - 1;
+                }
             }
             // Conduct another turn
             self.conduct_turn();
-
-            count += 1;
-            if (count % 1000000) == 0 {
-                println!("Conducted {} turns ...", count);
-            }
         }
     }
 }
@@ -85,14 +97,12 @@ fn solve_part_1(input: &String) -> String {
     let mut recipe_board = RecipeBoard::new("37");
     // Conduct turns until at least 10 new recipes have been made
     let recipes_to_left = input.parse::<usize>().unwrap();
-    let result = recipe_board.conduct_turns_until(recipes_to_left);
-    return result;
+    return recipe_board.conduct_turns_until(recipes_to_left);
 }
 
 #[aoc(day14, part2)]
 fn solve_part_2(input: &String) -> usize {
     // Create new recipe board
     let mut recipe_board = RecipeBoard::new("37");
-    let recipes_to_left = recipe_board.conduct_turns_until_appearance(input);
-    return recipes_to_left;
+    return recipe_board.conduct_turns_until_appearance(input);
 }

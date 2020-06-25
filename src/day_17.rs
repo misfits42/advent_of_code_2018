0f861c19f8770f483109973e3cd3ddf8ef859353
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-25 22:16:23 +1000

    Tidied up commenting

--- src/day_17.rs
+++ src/day_17.rs
@@ -2,9 +2,11 @@ use std::fmt;
 use std::collections::HashMap;
 use regex::Regex;
 
+use enum_iterator::IntoEnumIterator;
+
 use super::utils::map::Point2D;
 
-#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+#[derive(Copy, Clone, Hash, PartialEq, Eq, IntoEnumIterator)]
 enum MapTile {
     Clay,
     WaterFlow,
@@ -20,24 +22,32 @@ struct ReservoirMap {
 }
 
 impl ReservoirMap {
+    /// Creates a new ReservourMap from the given raw map input.
     pub fn new(raw_map: &str) -> Self {
         let mut contents = HashMap::<Point2D, MapTile>::new();
+        // Create regex to match two variants of line from raw input
         let x_range_regex = Regex::new(r"y=(\d+), x=(\d+)..(\d+)").unwrap();
         let y_range_regex = Regex::new(r"x=(\d+), y=(\d+)..(\d+)").unwrap();
+        // Keep track of the min and max X and Y co-ords seen
         let mut max_y: i64 = i64::MIN;
         let mut min_y: i64 = i64::MAX;
         let mut max_x: i64 = i64::MIN;
         let mut min_x: i64 = i64::MAX;
+        // Process each line in raw input
         for line in raw_map.lines() {
+            // Check if line matches regex with X-coord having range
             if x_range_regex.is_match(line) {
                 for capture in x_range_regex.captures_iter(line) {
+                    // Extract co-ordinate values from the line
                     let y_loc = capture[1].parse::<i64>().unwrap();
                     let x_low = capture[2].parse::<i64>().unwrap();
                     let x_high = capture[3].parse::<i64>().unwrap();
+                    // Record new horizontal clay vein
                     for x in x_low..=x_high {
                         let new_clay_loc = Point2D::new(x, y_loc);
                         contents.insert(new_clay_loc, MapTile::Clay);
                     }
+                    // Check in observed co-ord limits need to be updated
                     if y_loc > max_y {
                         max_y = y_loc;
                     } else if y_loc < min_y {
@@ -49,13 +59,17 @@ impl ReservoirMap {
                     if x_high > max_x {
                         max_x = x_high;
                     }
+                    // Line should only match regex once
                     break;
                 }
+            // Otherwise, the line will match the regex with the Y-coord having a range
             } else {
                 for capture in y_range_regex.captures_iter(line) {
+                    // Extract co-ordinate values from regex capture groups
                     let x_loc = capture[1].parse::<i64>().unwrap();
                     let y_low = capture[2].parse::<i64>().unwrap();
                     let y_high = capture[3].parse::<i64>().unwrap();
+                    // Record new vertical clay vein
                     for y in y_low..=y_high {
                         let new_clay_loc = Point2D::new(x_loc, y);
                         contents.insert(new_clay_loc, MapTile::Clay);
@@ -71,10 +85,12 @@ impl ReservoirMap {
                     } else if x_loc > max_x {
                         max_x = x_loc;
                     }
+                    // Line should only match regex once
+                    break;
                 }
             }
         }
-        
+        // Create the new ReservoirMap from the data extracted from the raw input
         Self {
             contents: contents,
             min_y: min_y,
@@ -84,6 +100,7 @@ impl ReservoirMap {
         }
     }
 
+    /// Creates a duplicate instance of the ReservoirMap by cloning all fields.
     pub fn duplicate(&self) -> Self {
         Self {
             contents: self.contents.clone(),
@@ -94,12 +111,16 @@ impl ReservoirMap {
         }
     }
 
+    /// Flows water from spring at (x:500, y:0) until all applicable spaces are filled with water
+    /// at rest or flowing water.
     pub fn flow_water(&mut self) {
         // Start with spring at (x:500, y:0)
         let spring_loc = Point2D::new(500, 0);
         self.flow_water_dfs(spring_loc);
     }
 
+    /// Checks if the given location contains a tile that additional water can come to rest upon - 
+    /// i.e. clay or other water at rest.
     fn check_if_full(&self, loc: Point2D) -> bool {
         if let Some(tile) = self.contents.get(&loc) {
             if *tile == MapTile::Clay || *tile == MapTile::WaterRest {
@@ -109,19 +130,21 @@ impl ReservoirMap {
         return false;
     }
 
+    /// Flows water down from the starting node, using a modified depth-first search algorithm
+    /// (adapted to meet needs to puzzle).
     fn flow_water_dfs(&mut self, node: Point2D) {
         // base case - exceeded max depth
         if node.pos_y > self.max_y {
             return;
         }
+        // Initially visit current node by inserting a water flow tile
         self.contents.insert(node, MapTile::WaterFlow);
-
         // Check if water can flow down - visit neighbour below
         let node_below = node.move_point(0, 1);
         if !self.check_if_full(node_below) {
             self.flow_water_dfs(node_below);
         }
-        // check if full below after recurse
+        // base case - no rest water under current node even after flow underneath
         if !self.check_if_full(node_below) {
             return;
         // Water cannot flow down
@@ -165,11 +188,13 @@ impl ReservoirMap {
             // If water is bounded on left and right, fill row and back-track
             let x_left_bound = left_bel_node.pos_x;
             let x_right_bound = right_bel_node.pos_x;
+            // If left and right bounds valid, insert water at rest between bounds then backtrack
             if is_left_bounded && is_right_bounded {
                 for x in x_left_bound..=x_right_bound {
                     self.contents.insert(Point2D::new(x, node.pos_y), MapTile::WaterRest);
                 }
                 return;
+            // Not bounded, so insert flowing water then flow down from sides where needed
             } else {
                 for x in x_left_bound..=x_right_bound {
                     self.contents.insert(Point2D::new(x, node.pos_y), MapTile::WaterFlow);
@@ -184,6 +209,7 @@ impl ReservoirMap {
         }
     }
 
+    /// Counts number of given tile type in the ReserviourMap.
     pub fn get_tile_count(&self, tile_type: MapTile) -> u64 {
         let mut count: u64 = 0;
         for (loc, tile) in self.contents.iter() {
